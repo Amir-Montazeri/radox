68967e7d5c1442e8815f156ad088333c7ce9e132
mahsa <m.moghaddami106@gmail.com>
2022-07-09 08:42:46 +0430

    published!

---
+++ src/components/navbar-components/index.jsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material";
+import { containerStyles } from "./navbarComponentsStyles";
+
+const NavbarComponents = () => {
+  return (
+    <>
+      <Grid item></Grid>
+      <Grid item></Grid>
+    </>
+  );
+};
+
+export default NavbarComponents;
