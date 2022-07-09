d3117b4b2f2c606b68715c5097d0efa3aa2355d0
mahsa <m.moghaddami106@gmail.com>
2022-07-09 08:19:42 +0430

    published

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { GlobalStyles } from "@mui/material";
+// import styles from "./globalStyles";
+
+const App = () => {
+  return (
+    <main>
+      {/* <GlobalStyles styles={styles} /> */}
+      <h1>Title</h1>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/globalStyles.js
@@ -0,0 +1,13 @@
+const styles = {
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box !important",
+  },
+  body: {
+    background: "#fff",
+    overflowX: "hidden",
+  },
+};
+
+export default styles;
