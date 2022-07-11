d78fa87e7384b963e0800dd7b19c5ae788a8accf
Amir-Montazeri <m3amir88@yahoo.com>
2022-07-11 14:50:30 +0430

    published!

--- src/components/auth-form/RenderedTextFields.jsx
+++ src/components/auth-form/RenderedTextFields.jsx
@@ -7,23 +7,47 @@ const RenderedTextFields = ({ items, values, setValue }) => {
     setValue({ ...values, [name]: value });
   };
 
-  return items.map(({ name, placeholder, type, icon }) => (
-    <Grid container justifyContent="center" key={name} sx={containerStyles}>
-      <Grid item>
-        <TextField
-          name={name}
-          value={values[name] ?? ""}
-          onChange={handleTextFieldChanged}
-          placeholder={placeholder}
-          type={type}
-          sx={textFieldStyles}
-        />
-      </Grid>
-      <Grid item>
-        <img src={icon} alt={name} height="100%" />
-      </Grid>
-    </Grid>
-  ));
+  return items.map(({ visibility, disabled = false, data }) =>
+    data.map(
+      ({
+        name,
+        placeholder,
+        type,
+        required = false,
+        pattern,
+        min,
+        max,
+        icon,
+      }) => (
+        <Grid
+          container
+          justifyContent="center"
+          key={name}
+          sx={{
+            ...containerStyles,
+            visibility,
+          }}
+        >
+          <Grid item>
+            <TextField
+              name={name}
+              value={values[name] ?? ""}
+              onChange={handleTextFieldChanged}
+              placeholder={placeholder}
+              inputProps={{ pattern, min, max }}
+              required={required}
+              type={type}
+              disabled={disabled}
+              sx={textFieldStyles}
+            />
+          </Grid>
+          <Grid item>
+            <img src={icon} alt={name} height="100%" />
+          </Grid>
+        </Grid>
+      )
+    )
+  );
 };
 
 export default RenderedTextFields;
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
 import axios from "axios";
 import { AuthComponents, AuthForm, RegisterTextfieldFooter } from "components";
 import { Home } from "pages";
 import Footer from "./Footer";
-import { buttonItems, textFieldItems } from "./sampleDatas";
+import { textFieldItems, renderOnCodeSent, buttonItems } from "./sampleDatas";
 
 const Login = () => {
+  const [codeSent, setCodeSent] = useState(false);
+  const extraTextFields = codeSent ? renderOnCodeSent : [];
+
   const footer = (
     <RegisterTextfieldFooter>
       <Footer />
     </RegisterTextfieldFooter>
   );
 
+  const handelSendingCode = (data) => {
+    const { phone_number } = data;
+    setCodeSent(true);
+    axios
+      .post(`${process.env.REACT_APP_BASE_API_LINK}`, {
+        phone_number,
+      })
+      .then((res) => {
+        if (res.code === "1") {
+          setCodeSent(true);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   const handleSubmit = (data) => {
     const { username, phoneNumber } = data;
+    console.log("submited: ", data);
     axios
       .post(`${process.env.REACT_APP_BASE_API_LINK}`, {
         username,
@@ -30,10 +52,22 @@ const Login = () => {
     <Home>
       <AuthComponents title="ورود کاربر">
         <AuthForm
-          textFieldItems={textFieldItems}
+          textFieldItems={[
+            {
+              status: "oke",
+              disabled: codeSent ? true : false,
+              data: textFieldItems,
+            },
+            {
+              visibility: codeSent ? "visible" : "hidden",
+              data: extraTextFields,
+            },
+          ]}
           textFieldFooters={footer}
           buttonItems={buttonItems}
-          formSubmited={handleSubmit}
+          formSubmited={(data) =>
+            !codeSent ? handelSendingCode(data) : handleSubmit(data)
+          }
         />
       </AuthComponents>
     </Home>
