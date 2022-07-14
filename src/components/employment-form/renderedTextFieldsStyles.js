428193296cc5cc26c8d4d7da433e5b65b9f0140e
Amir-Montazeri <m3amir88@yahoo.com>
2022-07-14 13:40:30 +0430

    published!

--- src/components/employment-form/renderedTextFieldsStyles.js
+++ src/components/employment-form/renderedTextFieldsStyles.js
@@ -2,21 +2,22 @@ export const containerStyles = {
     width: "46%",
     display: "flex",
     alignItems: "center",
-    marginBottom: "10px",
+    marginBottom: "15px",
     "@media screen and (max-width: 720px)": {
       width: "100%",
     },
   },
   textFieldsStyles = {
+    marginRight: "5px",
     width: "100%",
     height: "47.36px",
-    marginRight: "5px",
     direction: "rtl",
-    "> .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root, input": {
-      height: "100%",
-      color: "#4a4a4a",
-      bgcolor: "#F2F2F2",
-    },
+    "> .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root, .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input, input":
+      {
+        height: "100%",
+        color: "#4a4a4a",
+        bgcolor: "#F2F2F2",
+      },
     "fieldset.css-1d3z3hw-MuiOutlinedInput-notchedOutline": {
       borderColor: "#F2F2F2 !important",
     },
