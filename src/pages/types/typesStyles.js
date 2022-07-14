afc594f37474ec3e11ebfed5a8a2c15d2b6d4756
Amir-Montazeri <m3amir88@yahoo.com>
2022-07-14 12:14:40 +0430

    height fixed

--- src/pages/types/typesStyles.js
+++ src/pages/types/typesStyles.js
@@ -1,8 +1,11 @@
 export const containerStyles = {
-  height: "100vh",
+  height: "calc(100vh - 100px)",
   width: "100vw",
   backgroundRepeat: "no-repeat !important",
   backgroundSize: "cover !important",
   backgroundPosition: "center !important",
   backgroundColor: "#000 !important",
+  "@media screen and (max-width: 730px)": {
+    height: "calc(100vh - 150px)",
+  },
 };
